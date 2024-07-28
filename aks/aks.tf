d0e4c4b77100e2bf16e8ca19565900d421837a47
natut <infinity.enoch@gmail.com>
2024-07-28 15:31:48 +0800

    Added ACR and set only to plan

--- aks/aks.tf
+++ aks/aks.tf
@@ -107,3 +107,25 @@ resource "azurerm_kubernetes_cluster_node_pool" "spot" {
     ignore_changes = [node_count]
   }
 }
+
+
+
+resource "azurerm_container_registry" "acr" {
+  name                = "${local.env}${local.eks_name}acr"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name  
+  sku                 = "Standard"
+  admin_enabled       = true
+
+  tags = {
+    environment = local.env
+  }
+}
+
+# add the role to the identity the kubernetes cluster was assigned
+resource "azurerm_role_assignment" "kubweb_to_acr" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+  skip_service_principal_aad_check = true
+}
